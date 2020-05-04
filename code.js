fc47707c785af3579440fae224245e89b174b930
hana <hanalanzkowsky@gmail.com>
2020-05-04 19:29:25 -0400

    a

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
-var ezra = document.getElementByClassName("ek")
+// var ezra = document.getElementsByClassName("ek");
+
+// ezra.onclick = function(){
+// 	console.log("we workin!");
+// };
+
+
+$("#koenig").click(function(){
+console.log("hi");
+
+});
